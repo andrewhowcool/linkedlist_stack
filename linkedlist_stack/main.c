15bf4dceda99112b9a0b05ea18d1e15ef6529ac4
楊博名 <AndrewHowCool@yangbomingde-MacBook-Air.local>
2016-11-14 22:33:57 +0800

    Initial Commit

---
+++ linkedlist_stack/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  linkedlist_stack
+//
+//  Created by 楊博名 on 2016/11/14.
+//  Copyright © 2016年 楊博名. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
